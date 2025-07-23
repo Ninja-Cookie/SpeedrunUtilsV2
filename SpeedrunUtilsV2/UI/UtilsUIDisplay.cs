c5c374daa604076c1c4925370523d189dfec38f8
Ninja-Cookie <noreply>
2025-07-23 04:17:43 +0100

    Put text in its own area

--- SpeedrunUtilsV2/UI/UtilsUIDisplay.cs
+++ SpeedrunUtilsV2/UI/UtilsUIDisplay.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using UnityEngine;
 using static SpeedrunUtilsV2.UI.UtilsUI;
+using static SpeedrunUtilsV2.UI.UtilsUIText;
 
 namespace SpeedrunUtilsV2
 {
@@ -8,28 +9,28 @@ namespace SpeedrunUtilsV2
     {
         internal static void GUIFront(int windowID)
         {
-            string fpsHeader = CurrentFPSStatus == FPSStatus.Uncapped ? "<color=red>Uncapped</color>" : $"<color=silver>{Application.targetFrameRate}</color>";
+            string fpsHeader = CurrentFPSStatus == FPSStatus.Uncapped ? $"<color=red>{s_uncapped}</color>" : $"<color=silver>{Application.targetFrameRate}</color>";
             GUILabel($"({fpsHeader}) FPS: {FPS}", windowPropertiesMain);
 
             if (LiveSplitConfig.ActionKeys.TryGetValue(LiveSplitConfig.Actions.UncapFPS, out var action_uncap) && LiveSplitConfig.ActionKeys.TryGetValue(LiveSplitConfig.Actions.LimitFPS, out var action_fps))
-                GUILabel($"Uncap Framerate: <color=orange>{action_uncap}</color> | Limit Framerate to {LiveSplitConfig.SETTINGS_LimitValue.Item2}: <color=orange>{action_fps}</color>", windowPropertiesMain, true);
+                GUILabel($"{s_uncappedFramerate}: <color=orange>{action_uncap}</color> | {s_limitFramerateTo} {LiveSplitConfig.SETTINGS_LimitValue.Item2}: <color=orange>{action_fps}</color>", windowPropertiesMain, true);
 
-            GUILabel(ConnectionManager.IsConnected ? "<color=lime>Connected to LiveSplit!</color>" : "<color=red>Disconnected from LiveSplit</color>", windowPropertiesMain);
+            GUILabel(ConnectionManager.IsConnected ? $"<color=lime>{s_connected}</color>" : $"<color=red>{s_disconnected}</color>", windowPropertiesMain);
 
-            if (GUIButton(ConnectionManager.IsConnected ? "Disconnect from LiveSplit" : "Connect to LiveSplit", ConnectionManager.IsConnected ? Setup.ButtonType.On : Setup.ButtonType.Off, windowPropertiesMain))
+            if (GUIButton(ConnectionManager.IsConnected ? $"{s_disconnect}" : $"{s_connect}", ConnectionManager.IsConnected ? Setup.ButtonType.On : Setup.ButtonType.Off, windowPropertiesMain))
                 ToggleLiveSplit();
 
-            if (GUIButton("Splits", GUI_SPLITS_ENABLED ? Setup.ButtonType.On : Setup.ButtonType.Off, windowPropertiesMain))
+            if (GUIButton($"{s_splits}", GUI_SPLITS_ENABLED ? Setup.ButtonType.On : Setup.ButtonType.Off, windowPropertiesMain))
                 GUI_SPLITS_ENABLED = !GUI_SPLITS_ENABLED;
 
-            if (GUIButton("Set Max FPS", Setup.ButtonType.Normal, windowPropertiesMain))
+            if (GUIButton($"{s_setMax}", Setup.ButtonType.Normal, windowPropertiesMain))
                 SetMaxFPS(_fpsInt);
 
             FPSfield = GUIField(FPSfield, windowPropertiesMain);
 
             GUIEmptySpace(8f, windowPropertiesMain);
 
-            if (GUIButton("Credits", GUI_CREDITS_ENABLED ? Setup.ButtonType.On : Setup.ButtonType.Off, windowPropertiesMain))
+            if (GUIButton($"{s_credits}", GUI_CREDITS_ENABLED ? Setup.ButtonType.On : Setup.ButtonType.Off, windowPropertiesMain))
                 GUI_CREDITS_ENABLED = !GUI_CREDITS_ENABLED;
 
             Cleanup(windowPropertiesMain);
@@ -45,16 +46,16 @@ namespace SpeedrunUtilsV2
 
         internal static void GUICredits(int windowID)
         {
-            GUILabel("- Development -", windowPropertiesCredits);
+            GUILabel($"- {s_development} -", windowPropertiesCredits);
             GUILabel("<color=#FA7DE5>Ninja Cookie</color>", windowPropertiesCredits);
             GUILabel("<color=#800080>Loomeh</color>", windowPropertiesCredits);
             GUIEmptySpace(5f, windowPropertiesCredits);
 
-            GUILabel("- Previous Research -", windowPropertiesCredits);
+            GUILabel($"- {s_research} -", windowPropertiesCredits);
             GUILabel("<color=#9FA1A4>Jomoko</color>", windowPropertiesCredits);
             GUIEmptySpace(5f, windowPropertiesCredits);
 
-            GUILabel("- Original Tools -", windowPropertiesCredits);
+            GUILabel($"- {s_originalTools} -", windowPropertiesCredits);
             GUILabel("<color=#45BF60>Judah Caruso</color>", windowPropertiesCredits);
 
             Cleanup(windowPropertiesCredits);
